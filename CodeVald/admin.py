ceb2048bc8a075e162fc0d8a5241f26033180b4e
Anthony <Tony@ANTHONYs-MacBook-Pro.local>
2014-03-20 12:45:44 +0300

    Setup

---
+++ CodeVald/admin.py
@@ -0,0 +1,21 @@
+__author__ = 'Tony'
+from django.contrib import admin
+from CodeVald.models import Question
+from CodeVald.models import Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
