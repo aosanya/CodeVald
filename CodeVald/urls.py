203b477f0861b29baa8100cd064f4dd801e76597
aosanya <amosanya@gmail.com>
2014-03-23 10:37:26 +0300

    Hardcore Preview

--- CodeVald/urls.py
+++ CodeVald/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from CodeVald import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CodeVald.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^codevaldapp/', include('codevaldapp.urls', namespace="codevaldapp")),
     url(r'^admin/', include(admin.site.urls)),
 )
